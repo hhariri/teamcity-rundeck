b04915fed88f69f37efbf142795d91f951acbf33
Pierre Deman <pdeman@ankama.com>
2016-07-28 14:56:45 +0200

    Parse output entries and print them

--- rundeck-agent/src/main/kotlin/com/hadihariri/teamcity/plugins/rundeck/agent/RunDeckAPIResponse.kt
+++ rundeck-agent/src/main/kotlin/com/hadihariri/teamcity/plugins/rundeck/agent/RunDeckAPIResponse.kt
@@ -1,14 +1,34 @@
 package com.hadihariri.teamcity.plugins.rundeck.agent
 
-import jetbrains.buildServer.util.PropertiesUtil
-import java.io.File
+import java.util.*
 
 /**
  * Created by hadihariri on 05/10/15.
  */
 data class RunDeckExecuteJobResponse(val code: Int, val result: String)
 
-data class RunDeckJobStatusResponse(val code: Int, val completed: Boolean = false, val execCompleted: Boolean = false, val hasFailedNodes: Boolean = false, val execState: String = "failed", val execDuration: Long = 0, val rawData: String = "")
-data class RunDeckOptions(val url: String, val authToken: String, val includeOutput: Boolean, val failBuild: Boolean, val waitFinish: Boolean, val jobId: String, val jobOptions: String, val filters: String)
+data class RunDeckJobStatusResponse(
+        val code: Int,
+        val completed: Boolean = false,
+        val execCompleted: Boolean = false,
+        val hasFailedNodes: Boolean = false,
+        val execState: String = "failed",
+        val execDuration: Long = 0,
+        val offset: Long = 0,
+        val lastModified: Long = 0,
+        val entries: List<String> = ArrayList(),
+        val rawData: String = ""
+)
+
+data class RunDeckOptions(
+        val url: String,
+        val authToken: String,
+        val includeOutput: Boolean,
+        val failBuild: Boolean,
+        val waitFinish: Boolean,
+        val jobId: String,
+        val jobOptions: String,
+        val filters: String
+)
 
 
